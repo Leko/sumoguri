8708eae472f0cfd9179d3af485dcc42772cc55a8
Leko <leko.noor@gmail.com>
2019-08-09 01:46:23 +0900

    test: add tests for lib

---
+++ src/lib/filename.spec.ts
@@ -0,0 +1,23 @@
+import { filenamify } from "./filename";
+import { QueueItem } from "../QueueItem";
+
+describe("filenamify", () => {
+  it("returns flat path when flat is true", () => {
+    const item = new QueueItem({
+      locale: "en-US",
+      viewport: [100, 100],
+      url: new URL("http://localhost/hoge/foo/bar")
+    });
+    const filename = filenamify(item, true);
+    expect(filename).toBe("en-US-100x100--hoge-foo-bar.png");
+  });
+  it("returns path when flat is false", () => {
+    const item = new QueueItem({
+      locale: "en-US",
+      viewport: [100, 100],
+      url: new URL("http://localhost/hoge/foo/bar")
+    });
+    const filename = filenamify(item, false);
+    expect(filename).toBe("en-US/100x100/hoge/foo/bar.png");
+  });
+});
---
+++ src/lib/prettify.spec.ts
@@ -0,0 +1,8 @@
+import { prettify } from "./prettify";
+
+describe("prettify", () => {
+  it("should trim trailing slash", () => {
+    const url = new URL("http://localhost/about/");
+    expect(prettify(url)).toBe("/about");
+  });
+});
