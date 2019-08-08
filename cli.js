5987a28b27066071433850e621423131a95ae239
Leko <leko.noor@gmail.com>
2019-08-08 14:08:53 +0900

    chore: add polyfill for Array.prototype.{flat,flatMap}

--- cli.js
+++ cli.js
@@ -1,9 +1,10 @@
-const chalk = require('chalk')
-const { main } = require('./dist/cli/entry')
-const { parse } = require('./dist/cli/options')
+require("core-js/features/array/flat");
+require("core-js/features/array/flat-map");
+const chalk = require("chalk");
+const { main } = require("./dist/cli/entry");
+const { parse } = require("./dist/cli/options");
 
-main(parse())
-    .catch(e => {
-        console.error(chalk.red(e.stack))
-        process.exit(1)
-    })
+main(parse()).catch(e => {
+  console.error(chalk.red(e.stack));
+  process.exit(1);
+});
