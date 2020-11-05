fe00df00137874e5d3cbc3152768bfefbfd397a1
Leko <leko.noor@gmail.com>
2020-11-06 02:28:09 +0800

    Add CLI options test

--- src/cli/options.ts
+++ src/cli/options.ts
@@ -15,66 +15,71 @@ export type CLIOptions = {
   disableCssAnimation: boolean
 }
 
-const cmd = Yargs.options({
-  outDir: {
-    alias: 'o',
-    description: 'Output directory.',
-    default: '__screenshots__',
-  },
-  flat: {
-    alias: 'f',
-    type: 'boolean',
-    description: 'Flatten output filename.',
-    default: false,
-  },
-  include: {
-    alias: 'i',
-    description: 'Including paths.',
-    type: 'array',
-    default: [],
-  },
-  exclude: {
-    alias: 'e',
-    description: 'Excluding paths.',
-    type: 'array',
-    default: [],
-  },
-  depth: {
-    alias: 'd',
-    description: 'Depth of traversing.',
-    default: 100,
-    type: 'number',
-  },
-  viewports: {
-    alias: 'V',
-    description: 'Viewports.',
-    default: '800x600',
-    coerce: (args: string) => {
-      return args
-        .split(',')
-        .map((arg) => arg.split('x').map((n) => parseInt(n, 10)))
+export const cmd = Yargs.scriptName('sumoguri')
+  .usage('Usage: $0 <entry_url> [options]')
+  .example('$0 https://example.com', 'Run with default settings')
+  .example(
+    '$0 https://example.com --viewports 1680x1050,828x1792',
+    'Test in multiple viewports'
+  )
+  .example(
+    '$0 https://example.com --locales en-US,ja-JP',
+    'Test in multiple locales'
+  )
+  .example(
+    '$0 https://example.com --prefers-color-schemes=light,dark',
+    'Test in multiple color schemes'
+  )
+  .example('$0 https://example.com --depth 0', 'Do not traverse links')
+  .epilog('repository: https://github.com/Leko/sumoguri')
+  .options({
+    outDir: {
+      alias: 'o',
+      description: 'Output directory.',
+      default: '__screenshots__',
     },
-  },
-  locales: {
-    alias: 'l',
-    description: 'Locales.',
-    default: 'en-US',
-    coerce: (args: string) => {
-      return args.split(',').map((arg) => arg.trim())
+    flat: {
+      alias: 'f',
+      type: 'boolean',
+      description: 'Flatten output file structure.',
+      default: false,
     },
-  },
-  prefersColorSchemes: {
-    description: 'Specify prefers-color-scheme.',
-    default: 'light',
-    coerce: (args: string) => {
-      return args.split(',').map((arg) => arg.trim())
+    depth: {
+      alias: 'd',
+      description: 'Depth of traversing.',
+      default: 100,
+      type: 'number',
     },
-  },
-  disableCssAnimation: {
-    description: 'Disable CSS animation and transition.',
-    type: 'boolean',
-    default: true,
-  },
-})
+    viewports: {
+      alias: 'V',
+      description: 'Viewports.',
+      default: '800x600',
+      coerce: (args: string) => {
+        return args
+          .split(',')
+          .map((arg) => arg.split('x').map((n) => parseInt(n, 10)))
+      },
+    },
+    locales: {
+      alias: 'l',
+      description: 'Locales.',
+      default: 'en-US',
+      coerce: (args: string) => {
+        return args.split(',').map((arg) => arg.trim())
+      },
+    },
+    'prefers-color-schemes': {
+      description: 'Specify prefers-color-scheme.',
+      default: 'light',
+      coerce: (args: string) => {
+        return args.split(',').map((arg) => arg.trim())
+      },
+    },
+    'disable-css-animation': {
+      description: 'Disable CSS animation and transition.',
+      type: 'boolean',
+      default: true,
+    },
+  })
 
 export const parse = cmd.parse.bind(cmd)
